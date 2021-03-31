12fc91c878aec680c86c8bb25df32268d4e66a12
sbelikov98 <sbelikov@isgneuro.com>
2021-03-31 15:42:51 +0300

    Updated package.json and rollup.config.js files

--- DTCD-PrimitivePropertiesPanel/rollup.config.js
+++ DTCD-PrimitivePropertiesPanel/rollup.config.js
@@ -1,48 +1,42 @@
-import path from 'path';
-
+import vue from 'rollup-plugin-vue2';
 import alias from '@rollup/plugin-alias';
-import commonjs from '@rollup/plugin-commonjs';
 import styles from 'rollup-plugin-styles';
-import image from '@rollup/plugin-image';
-import vue from 'rollup-plugin-vue';
 import replace from '@rollup/plugin-replace';
+
+import path from 'path';
 
 const watch = Boolean(process.env.ROLLUP_WATCH);
 
 const pluginName = 'PrimitivePropertiesPanel';
-
-const projectSrcDir = path.join(path.resolve(__dirname), 'src');
-
-const fileDest = watch ? `./../../DTCD/server/plugins/DTCD-${pluginName}/${pluginName}.js` : `./build/${pluginName}.js`;
+const outputFile = `${pluginName}.js`;
+const outputDirectory = watch ? `./../../DTCD/server/plugins/DTCD-${pluginName}` : `./build`;
 
 const plugins = [
-	commonjs(),
-	alias({
-		entries: {
-			'@': projectSrcDir,
-		},
-	}),
-	vue(),
-	image(),
-	replace({
-		preventAssignment: true,
-		'process.env.NODE_ENV': JSON.stringify('development'),
-		'process.env.VUE_ENV': JSON.stringify('browser'),
-	}),
-	styles({
-		mode: 'inject',
-	}),
+  vue(),
+  alias({
+    entries: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  }),
+  styles({
+    mode: 'inject',
+  }),
+  replace({
+    preventAssignment: true,
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env.VUE_ENV': JSON.stringify('browser'),
+  }),
 ];
 
 export default {
-	input: `src/${pluginName}`,
-	output: {
-		file: fileDest,
-		format: 'esm',
-		sourcemap: false,
-	},
-	watch: {
-		include: ['./src/**'],
-	},
-	plugins,
+  plugins,
+  input: `src/PrimitivePropertiesPanel.js`,
+  output: {
+    file: `${outputDirectory}/${outputFile}`,
+    format: 'esm',
+    sourcemap: false,
+  },
+  watch: {
+    include: ['./src/**'],
+  },
 };
